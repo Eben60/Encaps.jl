0cbe3acbcfe0b432ea90408eb272aee5c08f04b1
Eben60 <8xe8n-cm@online.de>
2022-01-30 12:16:48 +0100

    link added

--- src/encaps.jl
+++ src/encaps.jl
@@ -1,3 +1,24 @@
+# https://discourse.julialang.org/t/parsimonious-way-to-unpack-big-structs/66522/11
+
 module encaps
 
+macro unp(p)
+    local nt = eval(p)
+    for k in keys(nt)
+        local v = nt[k]
+        @eval $k = $v
+    end
+end
+
+function upd(p; kwargs...)
+    p = Dict(pairs(p))
+    for kw in kwargs
+        p[kw.first] = kw.second
+    end
+    return (; p...)
+end
+
+export unp, upd
+
 end # module
+]
